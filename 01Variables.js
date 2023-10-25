fca4ebef44436e545e4c8bb61c0eabe0cc713416
Vojtech Vacl <vaclvojtech@gmail.com>
2023-10-25 11:47:03 +0200

    prvni hodina

---
+++ 01Variables.js
@@ -0,0 +1,17 @@
+let message;
+
+message = "Hello" // store the string
+
+console.log(message) // shows the variable content
+
+//Variable in one line
+
+let message2 = "Hello, World!"
+
+console.log(message2)
+
+//var vs let 
+
+var message3 = "Hello, People!" // old 
+
+console.log(message3)
