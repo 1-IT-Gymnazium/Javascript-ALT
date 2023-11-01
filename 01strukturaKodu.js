2b3a09007dcc19166758a57a826d7b90b0947f71
1124 <1124@student.itgymnazium.cz>
2023-11-01 11:31:31 +0100

    commit

---
+++ 01strukturaKodu.js
@@ -0,0 +1,17 @@
+console.log("Hello"); console.log("World");
+
+console.log("Hello");
+console.log("World");
+
+console.log("World")
+console.log("World")
+
+console.log(3 +
+    1
+    + 2)
+// jednořádkový koment
+/*
+více
+řádkový
+komentář
+*/
---
+++ 01variables.js
@@ -0,0 +1,40 @@
+let message;
+
+message = "Hello"
+
+console.log(message)
+
+// proměnná v jednom řádku
+
+let message2 = "Ahoj"
+
+console.log(message2)
+
+// var vs let
+
+var message3 = "Zdar" //var nepoužívat!!
+
+console.log(message3)
+
+// jmeno opromenne
+// jen pismena,  číslice, $, _
+
+let $d = 5
+
+console.log($d)
+
+let adminUser = "Chuck Norris"
+
+// konstanty
+
+const MY_BIRTHDAY = "1.7.1991"
+
+console.log(MY_BIRTHDAY = "1.7.1991")
+
+// MY_BIRTHDAY = "1.8.1970" vyhodi chybu
+
+console.log(MY_BIRTHDAY)
+
+let actorName = "Arnold"
+
+let shoppingCart = []
