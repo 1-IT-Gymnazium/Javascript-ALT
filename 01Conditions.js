f519c18ec33a3fd857bf241280354fed47781f91
Vojta Vacl <100764070+vaclvojta@users.noreply.github.com>
2023-12-06 10:42:27 +0100

    node.js not instaled

---
+++ 01Conditions.js
@@ -0,0 +1,25 @@
+//Condition
+// if (condition) {
+//     // code
+// }
+
+let year = 2005;
+if (year == 2005) {
+    console.log("You are 2000s kid");
+}
+
+if (""){
+    console.log("Nothing")
+} else {
+    console.log("Something")
+}
+let age = 15;
+if (age < 18) {
+    console.log("You cannot drink alcohol");
+} else if (age < 0) {
+    console.log("Non valid age");
+} else if (age > 120) {
+    console.log("What planet are you from?");
+} else {
+    console.log("You can drink alcohol");
+}
